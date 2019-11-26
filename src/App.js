53147dc85cae7edc217b40c70d153728d95e2639
Sahil Sharma <sahilaug@gmail.com>
2019-11-26 16:32:13 +0530

    wire tests in the app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import data from './data';
 
-export default () => {
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
-  const [result, setResult] = useState(0);
+export default () => (
+  <div className="card-container">
+    {
+      data.map((d) => <Card {...d} />)
+    }
+  </div>
+);
 
-  const onClick = () => {
-    const sum = num1 + num2;
-    setResult(sum);
-  };
-
+const Card = ({ show }) => {
+  const { name, image, summary } = show;
+  const { medium } = image;
+  const [isFav, setFav] = useState(false);
+  const [isExpanded, setExpanded] = useState(false);
   return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <Input name="num1" value={num1} onChange={(val) => setNum1(val)} />
-      <Input name="num2" value={num2} onChange={(val) => setNum2(val)} />
-      Result:
-      <span data-testid="result">{result}</span>
-      <div style={{ padding: '10px' }}>
-        <Button text="Add" onClick={onClick} />
+    <div className="card p20">
+      <img alt={name} src={medium} className="card-banner-img" />
+      <div>
+        <span className="flex-space-between">
+          {name}
+          <FontAwesomeIcon
+            icon="heart"
+            style={{ color: isFav ? 'red' : '' }}
+            onClick={() => setFav(!isFav)}
+          />
+        </span>
+      </div>
+      <div>
+        <span className="flex-space-between">
+          Summary
+          <FontAwesomeIcon
+            icon={isExpanded ? 'angle-up' : 'angle-down'}
+            onClick={() => setExpanded(!isExpanded)}
+          />
+        </span>
+        <div>
+          {isExpanded ? summary : null}
+        </div>
       </div>
     </div>
   );
 };
-
-const Input = ({ name, onChange, value }) => (
-  <div style={{ padding: '10px' }}>
-    <label>
-      {name}
-      <input
-        value={value}
-        onChange={(e) => onChange(parseInt(e.target.value, 10))}
-      />
-    </label>
-  </div>
-);
-
-const Button = ({ text, onClick }) => (
-  <button
-    type="submit"
-    onClick={onClick}
-  >
-    {text}
-
-  </button>
-);
-
-export { Input, Button };
--- src/App.test.js
+++ src/App.test.js
@@ -2,23 +2,3 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App, { Button, Input } from './App';
 import '@testing-library/jest-dom/extend-expect';
-
-/*
-  TESTS FOR BUTTON
-*/
-
-// it should be rendered with correct props
-// it should call the click handler when clicked
-
-/*
-  TESTS FOR INPUT COMPONENT
-*/
-
-// it should render with correct props
-// on typing in the input element, the text inside should change
-
-/*
-  TESTS FOR APP COMPONENT
-*/
-// it should render correctly (should have two input elements, a button element, and a result text)
-// it should be able to add two numbers correctly
